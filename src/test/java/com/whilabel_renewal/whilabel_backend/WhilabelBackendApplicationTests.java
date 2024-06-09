31d1abd7344149bfbcda543a42f3665622e61866
sangmin han <sangriel@naver.com>
2024-06-09 17:31:54 +0900

    프로젝트 생성

---
+++ src/test/java/com/whilabel_renewal/whilabel_backend/WhilabelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whilabel_renewal.whilabel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhilabelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
